0dd1b8b924f433c841c9f917c440308d959ca828
Scott Stokes <sstokes99@gmail.com>
2020-07-30 10:30:22 -0500

    Navigation Buttons working

--- src/pages/student-view/student-view.page.jsx
+++ src/pages/student-view/student-view.page.jsx
@@ -39,37 +39,68 @@ class StudentPage extends React.Component {
       };
     }
   
-    nextStep = () => {
-      var nextStep = this.state.currentStep + 1;
-      if ((nextStep + 1) >= this.props.currentLab.steps.length) {
-        this.setState({disableNextStep: true});
-      } 
-      this.setState({currentStep: nextStep});
-  
-      if ( nextStep > 0) {
-        this.setState({disablePreviousStep: false});
+    gotoNextStep = () => {
+      //
+      // Setup some needed variables to make the code more readable
+      //
+      var currentStep = this.state.currentStep;
+      var nextStep = currentStep + 1;
+      // var lastStep = this.state.currentLab.steps.length;
+      var willBeOnLastStep = ( nextStep + 1 ) >= this.props.currentLab.steps.length;
+      var willBeOnFirstStep = nextStep === 0;
+
+      if (willBeOnLastStep) {
+        this.setState({disableNextStep: true}); // No more steps, disable the button
       } else {
-        this.setState({disablePreviousStep: true});
+        this.setState({disableNextStep: false});  // More steps left, enable the button
       }
+  
+      if ( willBeOnFirstStep ) {
+        this.setState({disablePreviousStep: true});  // Enable the button
+      } else {
+        this.setState({disablePreviousStep: false});   // Disable the button
+      }
+
+      this.setState({currentStep: nextStep});   // Point to the next step
+
     }
   
-    previousStep = () => {
+    gotoPreviousStep = () => {
+      //
+      // Setup some needed variables to make the code more readable
+      //
+      var currentStep = this.state.currentStep;
+      var nextStep = currentStep + 1;
+      var areOnLastStep = (nextStep + 1) > this.props.currentLab.steps.length;
+
       var previousStep = this.state.currentStep - 1;
-      if ((previousStep + 1) >= this.props.currentLab.steps.length) {
-        this.setState({disableNextStep: true});
-      } else 
-      this.setState({currentStep: previousStep});
-      if ( previousStep > 0) {
-        this.setState({disablePreviousStep: false});
+
+      // See if there are more steps and adjust the NEXT button
+      if (areOnLastStep) {
+        console.log("gotoPreviousStep: On Last Step");
+        this.setState({disableNextStep: true});     // No more steps, disable the button
       } else {
-        this.setState({disablePreviousStep: true});
-        this.setState({disableNextStep: false});
+        console.log("gotoPreviousStep: more Steps left");
+        this.setState({disableNextStep: false});    // More steps left, enable the button
       }
+      
+      // See if there are previous steps and adjust the button
+      if ( previousStep > 0) {
+        console.log("gotoPreviousStep: there are previous steps");
+        this.setState({disablePreviousStep: false});  // Not on the first step, enable the button
+        this.setState({currentStep: previousStep});   // set to the previous step
+      } else {
+        console.log("gotoPreviousStep: At the beginning/first");
+        this.setState({disablePreviousStep: true});   // On the FIRST step, so disable the button
+        this.setState({currentStep: previousStep});
+      }
+
+
     }
   
     previousButton() {
       return (
-        <Button className='btn btn-secondary btn-spacing' onClick={this.previousStep} disabled={this.state.disablePreviousStep}>Previous</Button>
+        <Button className='btn btn-secondary btn-spacing' onClick={this.gotoPreviousStep} disabled={this.state.disablePreviousStep}>Previous</Button>
       );
     }
   
@@ -102,10 +133,10 @@ class StudentPage extends React.Component {
         renderers={{ code: CodeBlock }}
         ></ReactMarkdown>
         </div>
-        <PreviousButton onClick={this.previousStep} disabled={this.state.disablePreviousStep}></PreviousButton>
+        <PreviousButton onClick={this.gotoPreviousStep} disabled={this.state.disablePreviousStep}></PreviousButton>
         <CopyTextButton step={this.props.currentLab.steps[this.state.currentStep]}></CopyTextButton>
         {this.checkResultsButton()}
-        <Button className='btn btn-primary btn-spacing' onClick={this.nextStep} disabled={this.state.disableNextStep}>Next</Button>
+        <Button className='btn btn-primary btn-spacing' onClick={this.gotoNextStep} disabled={this.state.disableNextStep}>Next</Button>
           </div>
       );
     }
