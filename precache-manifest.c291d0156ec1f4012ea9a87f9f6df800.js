59fe5938b2053b63d40232d362abc6c0715925b7
Scott Stokes <sstokes99@gmail.com>
2020-07-31 08:56:43 -0500

    Updates

---
+++ precache-manifest.c291d0156ec1f4012ea9a87f9f6df800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5014e24ae714915202892dab791831",
+    "url": "/eLaborate/index.html"
+  },
+  {
+    "revision": "d458f57dd9d469fefe9f",
+    "url": "/eLaborate/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "72109660aee254b85d00",
+    "url": "/eLaborate/static/css/main.e1594d5a.chunk.css"
+  },
+  {
+    "revision": "d458f57dd9d469fefe9f",
+    "url": "/eLaborate/static/js/2.3ad94516.chunk.js"
+  },
+  {
+    "revision": "352e3fc0cb0f5fef85ab91b9165b9cd1",
+    "url": "/eLaborate/static/js/2.3ad94516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72109660aee254b85d00",
+    "url": "/eLaborate/static/js/main.8812d562.chunk.js"
+  },
+  {
+    "revision": "ef0ab80d39cb0207a9b8",
+    "url": "/eLaborate/static/js/runtime-main.50b189da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eLaborate/precache-manifest.de903fef0ec6e660661f3a9b1936460a.js"
+  "/eLaborate/precache-manifest.c291d0156ec1f4012ea9a87f9f6df800.js"
 );
 
 self.addEventListener('message', (event) => {
